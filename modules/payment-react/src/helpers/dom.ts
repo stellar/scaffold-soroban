7ef26378a0e4d11b1c58d38f7c7caf92b667a766
Aristides Staffieri <aristides.staffieri@stellar.org>
2023-06-06 13:51:53 -0600

    correctly adds git submodule

--- modules/payment-react/src/helpers/dom.ts
+++
@@ -1,8 +0,0 @@
-export const copyContent = async (text: string) => {
-  try {
-    await navigator.clipboard.writeText(text);
-    console.log("Content copied to clipboard");
-  } catch (err) {
-    console.error("Failed to copy: ", err);
-  }
-};
